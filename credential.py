7d013cc2db184e64ec39cf9cab3246922db519c8
suneelmaan-lab <suneelmaan@gmail.com>
2025-09-25 23:09:39 +0530

    Update credential.py

--- credential.py
+++ credential.py
@@ -1,14 +1,1 @@
-# credentials.py
-
-# Device credentials
-username = '<your normal AD username>'
-password = '<your normal AD password>'
-
-jumphost1 = {
-    'device_type': 'linux',
-    'ip': 'x.y.z.x',
-    'username': '<your jumphost username>',
-    'password': '<your jumphost password>',
-    'port': 22,
-}
 
