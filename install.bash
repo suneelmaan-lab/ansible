aba101aa9ee7607b55720a2715edede463de7197
suneelmaan-lab <suneelmaan@gmail.com>
2025-09-25 23:00:58 +0530

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,14 @@
+pip3 install mysql-connector-python
+pip3 install json
+pip3 install pyats
+pip3 install genie
+pip3 install csv
+pip3 install pandas
+pip3 install netmiko
+pip3 install netmiko_multihop
+
+#if you are using Ubuntu in WSL
+#sudo apt install mysql-server
+
+#Start mysql
+#sudo /etc/init.d/mysql start
